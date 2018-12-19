a6ae75225e44454fd6a5541aed41fd5c07330603
Matthew Russell <mbr4477@gmail.com>
2018-12-19 09:02:46 -0500

    Formatting

--- src/main/kotlin/com/matthewrussell/trchunkbrowser/app/mainview/MainView.kt
+++ src/main/kotlin/com/matthewrussell/trchunkbrowser/app/mainview/MainView.kt
@@ -189,7 +189,7 @@ class MainView : View() {
                         label("${messages[segment.sourceMetadata.book]} ${segment.sourceMetadata.chapter}:${segment.label}") {
                             addClass(MainViewStyles.segmentTitle)
                         }
-                        val take= segment.src.nameWithoutExtension.split("_").filter { it.startsWith("t") }.last().substring(1)
+                        val take = segment.src.nameWithoutExtension.split("_").filter { it.startsWith("t") }.last().substring(1)
                         label("Take $take") {
                             addClass(MainViewStyles.segmentInfo)
                         }
